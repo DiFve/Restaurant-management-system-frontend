261d6365b0bd160096374b921497733ffaecea1f
kawjaw66 <kawjaw66@gmail.com>
2022-04-14 18:28:39 +0700

    +food data fetch

--- src/Pages/FoodPage.tsx
+++ src/Pages/FoodPage.tsx
@@ -2,17 +2,15 @@ import { useParams } from "react-router-dom"
 import HeaderBar from "../components/HeaderBar"
 import FoodForm from "../components/FoodForm"
 import placeholder from '../components/img/menu_placeholder.jpg'
+import { getFoodInfo, getFoodPic } from "../api/food"
+import { useEffect, useState } from "react"
 
 const FoodPage : React.FC = () =>{
     const {id} = useParams()
-    console.log(id)
     return (
-        <div className="flex flex-col h-screen w-full items-center">
+        <div className="flex flex-col h-screen w-full items-center overflow-y-hidden">
             <HeaderBar name="Im Ar Roi"/>
             <div className="flex flex-col items-center w-[90%] h-[80%] mt-[40px] border-black border-[2px]">
-                <div className='flex w-[100%] h-[30%] justify-center items-center'>
-                    <img src={placeholder} alt="menu_thumbnail" className='w-[90%] h-[80%]'/>
-                </div>
                 <FoodForm id={id}/>
             </div>
         </div>
--- src/components/FoodForm.tsx
+++ src/components/FoodForm.tsx
@@ -1,53 +1,42 @@
-import { MouseEventHandler, useState } from "react"
-
-interface Food{
+import { MouseEventHandler, useEffect, useState } from "react"
+import { getFoodInfo,getFoodPic } from "../api/food"
+import config from "../config.json";
+interface idProps{
     id:string | undefined
 }
-const FoodForm : React.FC<Food> = (props) =>{
-    var foodDetail:string = 'asasdfasdfasdfasasdfaghujwrjtdfasdf'
+const FoodForm : React.FC<idProps> = (props) =>{
     var obj = 
     { 
         "detail": 
         [
             {
                 "topicName": [
-                    "Spicy",
-                    "Happiness",
-                    "Taste",
                 ],
                 "choice": [
-                    "single",
-                    "single",
-                    "single"
                 ],
                 "option": [
-                    [
-                        "PhetNoi",
-                        "PhetKlang",
-                        "PhetMak"
-                    ],
-                    [
-                        "Rare",
-                        "Medium",
-                        "Medium-rare"
-                    ],
-                    [
-                        "Khem",
-                        "MaiKhem",
-                        "Khem muen hee"
-                    ]
                 ],
                 "require": [
-                        "true",
-                        "true",
-                        "false"
                 ],
-                "_id": "62543514d8df66ccde71ea08"
+                "_id": ""
             }
         ]
     }
+    const [foodInfo,setFoodInfo] = useState<any>(obj)
+    const [foodDetail,setFoodDetail] = useState<any>(obj.detail[0])
+    const [foodPic,setFoodPic] = useState('')
+    useEffect(() => {
+        const getFood = async () => {
+            const res = await getFoodInfo(props.id)
+            const foodData = res?.data
+            setFoodInfo(foodData.menu)
+            setFoodDetail(foodData.menu.detail[0])
+            setFoodPic(`${config.imageURL}${foodData.menu.image}`)
+        }
+        getFood()
+      }, []);
     const [number,setNumber] = useState(0)
-    var {topicName,choice,option,require,_id} = obj.detail[0]
+    var {topicName,choice,option,require,_id} = foodDetail
     const onNumberClickHandler=(event:any)=>{
         var action = event.target.id
         if(action == 'plus'){
@@ -59,63 +48,72 @@ const FoodForm : React.FC<Food> = (props) =>{
             }
         }
     }
-    console.log(topicName)
     return(
-        <div className="h-[70%] w-[100%]">
-            <div className="h-[20%] w-[100%] pl-[3%] pr-[4%] break-words overflow-y-scroll border-b-2">
-                <label className="text-2xl">Details : </label>
-                <p className="text-2xl">{foodDetail}</p>
+        <div>
+            <div className='flex w-[100%] h-[25%] justify-center items-center'>
+                <img src={foodPic} alt="menu_thumbnail" className='w-[90%] h-[80%]'/>
             </div>
-            <div className="flex flex-col h-[55%] w-[100%] mt-[2%] pl-[3%] pr-[4%] overflow-y-scroll">
-                <form className="text-2xl w-[100%]">
-                    {  
-                        topicName.map((val,index)=>{
-                            return(
-                                <fieldset id={'group ' + index}>
-                                    <label>{val}</label>
-                                    {
-                                        option[index].map((val)=>{
-                                            if(require[index]==="true"){
-                                                return(
-                                                    <div className="ml-[10%]">
-                                                        <input className='indent-3' type="radio" value={val} name={topicName[index]} required/>
-                                                        <label>{val}</label>
-                                                    </div>
-                                                )
-                                            }
-                                            else if(require[index]==='false'){
-                                                return(
-                                                    <div className="ml-[10%]">
-                                                        <input className='indent-3' type="radio" value={val} name={topicName[index]}/>
-                                                        <label>{val}</label>
-                                                    </div>
-                                                )
-                                            }
-                                        })
-                                    }
-                                </fieldset>
-                            )
-                        })
-                    }
-                    <label>Additional : </label>
-                    <textarea className="bg-gray-100 border border-black">
-                    </textarea>
-                </form>
-                    
-            </div>
-            <div className="flex flex-col h-[24%] w-[100%] overflow-y-scroll">
-                <div className="flex h-[50%] w-[100%] justify-center items-center text-4xl">
-                    <button className="flex bg-headerRed h-[100%] w-[17%] rounded-[50%]" id='minus' onClick={onNumberClickHandler}>-</button>
-                    <label className="ml-[20px] mr-[20px] w-[32px] text-center">{number}</label>
-                    <button className="flex bg-headerRed h-[100%] w-[17%] rounded-[50%]" id='plus' onClick={onNumberClickHandler}>+</button>
+            <div className="h-[70%] w-[100%]">
+                <div className="h-[10%] w-[100%] pl-[3%] pr-[4%] break-words border-b-2 text-center text-4xl">
+                    <label>{foodInfo.foodName}</label>
                 </div>
-                <div className="flex h-[50%] w-[100%] justify-center items-center text-center">
-                    <button className="flex bg-headerRed h-[75%] w-[50%] text-center text-3xl text-white justify-center items-center border-[2px]">
-                        <label htmlFor=""> Confirm </label>
-                    </button>
+                <div className="h-[15%] w-[100%] pl-[3%] pr-[4%] break-words overflow-y-scroll border-b-2">
+                    <label className="text-2xl">Details : </label>
+                    <p className="text-2xl">{foodInfo.description}</p>
+                </div>
+                <div className="flex flex-col h-[40%] w-[100%] mt-[2%] pl-[3%] pr-[4%] overflow-y-scroll border-b-2 ">
+                    <form className="text-2xl w-[100%]">
+                        {  
+                            topicName.map((val:any,index:number)=>{
+                                return(
+                                    <fieldset id={'group ' + index}>
+                                        <label>{val}</label>
+                                        {
+                                            option[index].map((val:any)=>{
+                                                if(require[index]==="true"){
+                                                    return(
+                                                        <div className="ml-[10%]">
+                                                            <input className='indent-3' type="radio" value={val} name={topicName[index]} required/>
+                                                            <label>{val}</label>
+                                                        </div>
+                                                    )
+                                                }
+                                                else if(require[index]==='false'){
+                                                    return(
+                                                        <div className="ml-[10%]">
+                                                            <input className='indent-3' type="radio" value={val} name={topicName[index]}/>
+                                                            <label>{val}</label>
+                                                        </div>
+                                                    )
+                                                }
+                                            })
+                                        }
+                                    </fieldset>
+                                )
+                            })
+                        }
+                        <label>Additional : </label>
+                        <textarea className="bg-gray-100 border border-black">
+                        </textarea>
+                    </form>
+                        
+                </div>
+                <div className="flex flex-col h-[24%] w-[100%] overflow-y-scroll">
+                    <div className="flex h-[50%] w-[100%] justify-center items-center text-4xl">
+                        <button className="flex bg-headerRed h-[85%] w-[15%] rounded-[50%]" id='minus' onClick={onNumberClickHandler}>-</button>
+                        <label className="ml-[20px] mr-[20px] w-[32px] text-center">{number}</label>
+                        <button className="flex bg-headerRed h-[85%] w-[15%] rounded-[50%]" id='plus' onClick={onNumberClickHandler}>+</button>
+                    </div>
+                    <div className="flex h-[50%] w-[100%] justify-center items-center text-center">
+                        <button className="flex bg-headerRed h-[75%] w-[50%] text-center text-3xl text-white justify-center items-center border-[2px]">
+                            <label htmlFor=""> Confirm </label>
+                        </button>
+                    </div>
                 </div>
             </div>
+
         </div>
+        
     )
 }
 
