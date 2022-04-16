c5296744c97fff2a966c038decc032d6f2245294
kawjaw66 <kawjaw66@gmail.com>
2022-04-17 00:50:23 +0700

    +added price in food page

--- src/components/FoodForm.tsx
+++ src/components/FoodForm.tsx
@@ -25,6 +25,8 @@ const FoodForm : React.FC<idProps> = (props) =>{
     const [foodInfo,setFoodInfo] = useState<any>(obj)
     const [foodDetail,setFoodDetail] = useState<any>(obj.detail[0])
     const [foodPic,setFoodPic] = useState('')
+    const [foodPrice,setFoodPrice] = useState(0)
+    const userFoodType:string = 'buffet'
     useEffect(() => {
         const getFood = async () => {
             const res = await getFoodInfo(props.id)
@@ -32,6 +34,12 @@ const FoodForm : React.FC<idProps> = (props) =>{
             setFoodInfo(foodData.menu)
             setFoodDetail(foodData.menu.detail[0])
             setFoodPic(`${config.imageURL}${foodData.menu.image}`)
+            if(userFoodType==='buffet' && foodData.menu.foodType.includes('buffet')){
+                setFoodPrice(0)
+            }
+            else{
+                setFoodPrice(foodData.menu.price)
+            }
         }
         getFood()
       }, []);
@@ -54,8 +62,16 @@ const FoodForm : React.FC<idProps> = (props) =>{
                 <img src={foodPic} alt="menu_thumbnail" className='w-[90%] h-[80%] max-h-[170px] min-h-[170px]'/>
             </div>
             <div className="h-[70%] w-[100%]">
-                <div className="h-[10%] w-[100%] pl-[3%] pr-[4%] break-words border-b-2 text-center text-4xl">
-                    <label>{foodInfo.foodName}</label>
+                <div className="flex flex-row w-full bg-red-200 border-b-2 items-center justify-center">
+                    <div className="h-[100%] w-[22.5%] break-words text-center text-4xl">
+                        
+                    </div>
+                    <div className="h-[100%] w-[55%] break-words text-center text-3xl">
+                        <label>{foodInfo.foodName}</label>
+                    </div>
+                    <div className="flex h-[100%] w-[22.5%] break-words text-center text-2xl justify-center items-center">
+                        <label>{foodPrice}&nbsp;฿</label>
+                    </div>
                 </div>
                 <div className="h-[15%] w-[100%] pl-[3%] pr-[4%] break-words overflow-y-scroll border-b-2">
                     <label className="text-2xl">Details : </label>
