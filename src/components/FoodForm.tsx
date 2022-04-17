1c3fe06f3bc3d3061f803ad81b12c1641267f801
Makasan <63010791@KMITL.AC.TH>
2022-04-17 23:00:23 +0700

    test

--- src/components/FoodForm.tsx
+++ src/components/FoodForm.tsx
@@ -1,32 +1,32 @@
 import { MouseEventHandler, useEffect, useState } from "react"
 import { getFoodInfo } from "../api/food"
 import config from "../config.json";
-interface idProps{
-    id:string | undefined
+interface idProps {
+    id: string | undefined
 }
-const FoodForm : React.FC<idProps> = (props) =>{
-    var obj = 
-    { 
-        "detail": 
-        [
-            {
-                "topicName": [
-                ],
-                "choice": [
-                ],
-                "option": [
-                ],
-                "require": [
-                ],
-                "_id": ""
-            }
-        ]
+const FoodForm: React.FC<idProps> = (props) => {
+    var obj =
+    {
+        "detail":
+            [
+                {
+                    "topicName": [
+                    ],
+                    "choice": [
+                    ],
+                    "option": [
+                    ],
+                    "require": [
+                    ],
+                    "_id": ""
+                }
+            ]
     }
-    const [foodInfo,setFoodInfo] = useState<any>(obj)
-    const [foodDetail,setFoodDetail] = useState<any>(obj.detail[0])
-    const [foodPic,setFoodPic] = useState('')
-    const [foodPrice,setFoodPrice] = useState(0)
-    const userFoodType:string = 'buffet'
+    const [foodInfo, setFoodInfo] = useState<any>(obj)
+    const [foodDetail, setFoodDetail] = useState<any>(obj.detail[0])
+    const [foodPic, setFoodPic] = useState('')
+    const [foodPrice, setFoodPrice] = useState(0)
+    const userFoodType: string = 'buffet'
     useEffect(() => {
         const getFood = async () => {
             const res = await getFoodInfo(props.id)
@@ -34,37 +34,37 @@ const FoodForm : React.FC<idProps> = (props) =>{
             setFoodInfo(foodData.menu)
             setFoodDetail(foodData.menu.detail[0])
             setFoodPic(`${config.imageURL}${foodData.menu.image}`)
-            if(userFoodType==='buffet' && foodData.menu.foodType.includes('buffet')){
+            if (userFoodType === 'buffet' && foodData.menu.foodType.includes('buffet')) {
                 setFoodPrice(0)
             }
-            else{
+            else {
                 setFoodPrice(foodData.menu.price)
             }
         }
         getFood()
-      }, []);
-    const [number,setNumber] = useState(0)
-    var {topicName,choice,option,require,_id} = foodDetail
-    const onNumberClickHandler=(event:any)=>{
+    }, []);
+    const [number, setNumber] = useState(0)
+    var { topicName, choice, option, require, _id } = foodDetail
+    const onNumberClickHandler = (event: any) => {
         var action = event.target.id
-        if(action == 'plus'){
-            setNumber(number+1)
+        if (action == 'plus') {
+            setNumber(number + 1)
         }
         else {
-            if(number > 0){
-                setNumber(number-1)
+            if (number > 0) {
+                setNumber(number - 1)
             }
         }
     }
-    return(
+    return (
         <div className="flex flex-col h-full">
             <div className='flex w-[100%] h-[25%] justify-center items-center'>
-                <img src={foodPic} alt="menu_thumbnail" className='w-[90%] h-[80%] max-h-[170px] min-h-[170px]'/>
+                <img src={foodPic} alt="menu_thumbnail" className='w-[90%] h-[80%] max-h-[170px] min-h-[170px]' />
             </div>
             <div className="h-[70%] w-[100%]">
                 <div className="flex flex-row w-full border-b-2 items-center justify-center">
                     <div className="h-[100%] w-[22.5%] break-words text-center text-4xl">
-                        
+
                     </div>
                     <div className="h-[100%] w-[55%] break-words text-center text-3xl font-bold">
                         <label>{foodInfo.foodName}</label>
@@ -79,26 +79,26 @@ const FoodForm : React.FC<idProps> = (props) =>{
                 </div>
                 <div className="flex flex-col h-[53%] w-[100%] mt-[2%] pl-[3%] pr-[4%] overflow-y-scroll border-b-2 min-h-[200px]">
                     <form className="text-2xl w-[100%]">
-                        {  
-                            topicName.map((val:any,index:number)=>{
-                                if(val !== ''){
-                                    return(
+                        {
+                            topicName.map((val: any, index: number) => {
+                                if (val !== '') {
+                                    return (
                                         <fieldset id={'group ' + index}>
                                             <label>{val}</label>
                                             {
-                                                option[index].map((val:any)=>{
-                                                    if(require[index]==="true"){
-                                                        return(
+                                                option[index].map((val: any) => {
+                                                    if (require[index] === "true") {
+                                                        return (
                                                             <div className="ml-[10%]">
-                                                                <input className='' type="radio" value={val} name={topicName[index]} required/>
+                                                                <input className='' type="radio" value={val} name={topicName[index]} required />
                                                                 <label>{val}</label>
                                                             </div>
                                                         )
                                                     }
-                                                    else if(require[index]==='false'){
-                                                        return(
+                                                    else if (require[index] === 'false') {
+                                                        return (
                                                             <div className="ml-[10%]">
-                                                                <input className='' type="radio" value={val} name={topicName[index]}/>
+                                                                <input className='' type="radio" value={val} name={topicName[index]} />
                                                                 <label>{val}</label>
                                                             </div>
                                                         )
@@ -108,10 +108,10 @@ const FoodForm : React.FC<idProps> = (props) =>{
                                         </fieldset>
                                     )
                                 }
-                                else{
-                                    return(
+                                else {
+                                    return (
                                         <div>
-                                            
+
                                         </div>
                                     )
                                 }
@@ -121,7 +121,7 @@ const FoodForm : React.FC<idProps> = (props) =>{
                         <textarea className="bg-gray-100 border border-black">
                         </textarea>
                     </form>
-                        
+
                 </div>
                 <div className="flex flex-col h-[24%] w-[100%] overflow-y-scroll">
                     <div className="flex h-[50%] w-[100%] justify-center items-center text-4xl text-center">
@@ -138,7 +138,7 @@ const FoodForm : React.FC<idProps> = (props) =>{
             </div>
 
         </div>
-        
+
     )
 }
 
