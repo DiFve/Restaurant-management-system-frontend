3b068174d8c3f5319e44a1e0e87243350655ff5c
phurits <phurits@hotmail.com>
2022-04-19 20:54:31 +0700

    เปิดปุ่มในหน้า employeeMain

--- src/Pages/EmployeeMainPage.tsx
+++ src/Pages/EmployeeMainPage.tsx
@@ -1,7 +1,6 @@
+import QueueLayout from "../components/QueueLayout"
 import HeaderBar from "../components/RestaurantManagerBar"
-import TableButton from "../components/TableButton"
-
-
+import TableLayout from "../components/TableLayout"
 
 
 const EmployeeMainPage : React.FC = () =>{
@@ -11,17 +10,19 @@ const EmployeeMainPage : React.FC = () =>{
             <HeaderBar name="EmployeeMain"></HeaderBar>
             <div className="bg-grey-100 flex flex-row ... h-full">
                 <div className="bg-orange-100 left-0 w-1/4 h-full overflow-auto">
-                    <div className="flex flex-col ...  text-3xl content-center ... text-center m-5 gap-5">
-                        <div className="bg-white border-solid border-2 border-black shadow-md hover:bg-slate-100 rounded-md ">01</div>
-                        <div className="border-solid border-2 border-black shadow-md">02</div>
+                <div className="my-3 bg-blue-100 text-3xl text-center">
+                        <h1>IN QUEUE</h1>
+                     </div>
+                    <div >
+                        <QueueLayout/>
                     </div>
                 </div>
-
-                 <div className="bg-red-100 right-0 w-3/4 h-screen overflow-auto">
-                    <div className="grid grid-cols-6 gap-3 m-3"> 
-                        <TableButton text="dlafkjasdkl;jf;" />
-                        
-                    </div>
+                 <div className="bg-red-100 right-0 w-3/4 h-screen overflow-auto"> {/* ด้านขวาฝั่ง TABLE */}
+                     <div className=" my-3 w-full bg-yellow-100 text-3xl text-center "> 
+                         
+                        <h1>TABLE</h1>
+                     </div>
+                    <TableLayout/>
                 </div>
             </div>
         </div>
