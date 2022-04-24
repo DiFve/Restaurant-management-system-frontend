acc16a983eb37e453f6af2e3aa001520397c958a
kawjaw66 <kawjaw66@gmail.com>
2022-04-25 03:05:26 +0700

    +fixed icon too large

--- src/components/EmployeeCall.tsx
+++ src/components/EmployeeCall.tsx
@@ -4,7 +4,7 @@ const EmployeeCall : React.FC = () =>{
 
     return(
         <div className='flex flex-col justify-center items-center'>
-            <img src={employee_icon} alt="" className='ml-[20%] mb-[5%] h-[40%]'/>
+            <img src={employee_icon} alt="" className='ml-[20%] mb-[5%] h-[40%] max-h-[390px]'/>
             <label className='text-3xl mb-[3%]'>Our Staff Is Coming</label>
             <label className='text-3xl mb-[3%]'>Please Wait</label>
             <div className='flex mb-[10%] justify-center items-center h-[10%]'>
