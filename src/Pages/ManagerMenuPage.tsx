15c2005805d8360fabd0483640f044a7b0a68cd4
Suratan63011017 <folnw2545@gmail.com>
2022-04-25 04:53:03 +0700

    thai language manager side

--- src/Pages/ManagerMenuPage.tsx
+++ src/Pages/ManagerMenuPage.tsx
@@ -2,17 +2,16 @@ import HeaderBar from "../components/RestaurantManagerBar";
 import MenuComponent from "../components/ManagerMenuComponent";
 import AddIcon from "../components/img/add_icon.jpg";
 import searchIcon from "../components/img/search_icon.png";
-import backIcon from "../components/img/back_icon.png"
+import backIcon from "../components/img/back_icon.png";
 import { useNavigate } from "react-router-dom";
 import { allMenu } from "../api/menu";
 import { useEffect, useState } from "react";
 
 const ManagerMenuPage: React.FC = () => {
   const [menu, setMenu] = useState([]);
-  const [menuTypeSelected, setMenuTypeSelected] = useState("all");
+  const [menuTypeSelected, setMenuTypeSelected] = useState("ทั้งหมด");
   const [search, setSearch] = useState("");
   const [fillMenu, setFillMenu] = useState<any>([]);
-
 
   const menuTypeChange = (event: any) => {
     setMenuTypeSelected(event.target.value);
@@ -27,9 +26,9 @@ const ManagerMenuPage: React.FC = () => {
     navigate("/ManagerMenu/addMenu");
   };
 
-  const backHandle = () =>{
-    navigate("/home")
-  }
+  const backHandle = () => {
+    navigate("/home");
+  };
 
   useEffect(() => {
     const getMenu = async () => {
@@ -44,7 +43,7 @@ const ManagerMenuPage: React.FC = () => {
   useEffect(() => {
     const filterChange = () => {
       var newMenu: any = [];
-      if (menuTypeSelected != "all") {
+      if (menuTypeSelected != "ทั้งหมด") {
         menu.filter((element: any) => {
           if (element.foodType.includes(menuTypeSelected)) {
             newMenu.push(element);
@@ -75,24 +74,24 @@ const ManagerMenuPage: React.FC = () => {
   return (
     <div>
       <div className="bg-white h-screen">
-        <HeaderBar name="Menu"></HeaderBar>
+        <HeaderBar name="เมนูทั้งหมด"></HeaderBar>
         <div className="flex flex-row w-full h-[75px] justify-between items-center bg-red-50">
           <select
             className="form-select block text-md font-normal text-gray-700 bg-white border h-[40px] ml-5
                     border-solid border-gray-300 rounded focus:text-gray-700 focus:border-blue-600 focus:outline-none"
             onChange={menuTypeChange}
           >
-            <option value="all" selected={menuTypeSelected === "all"}>
-              all
+            <option value="ทั้งหมด" selected={menuTypeSelected === "ทั้งหมด"}>
+              ทั้งหมด
             </option>
             <option
               value="a-la-carte"
               selected={menuTypeSelected === "a-la-carte"}
             >
-              a-la-carte
+              อาหารจานเดียว
             </option>
             <option value="buffet" selected={menuTypeSelected === "buffet"}>
-              buffet
+              บุฟเฟต์
             </option>
             );
           </select>
@@ -102,7 +101,7 @@ const ManagerMenuPage: React.FC = () => {
               type="text"
               className="flex w-auto h-[40px] px-3 py-1.5 text-md font-normal text-gray-700 mr-5
               border border-solid border-gray-300 rounded focus:border-black focus:outline-none"
-              placeholder="Search..."
+              placeholder="ค้นหา..."
               onChange={searchChange}
               value={search}
             />
